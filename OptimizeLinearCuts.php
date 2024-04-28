c221ef1d873dfa68cf0082b79125cd97a5ac8f55
stewart.fritts@gmail.com <stewart.fritts@gmail.com>
2024-04-28 09:15:11 -0500

    Removed index.php and created OptimizeLinearCuts.php class file

---
+++ OptimizeLinearCuts.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Description of OptimizeLinearCuts
+ * 
+ * Create report showing how to cut an assorted list of cut lengths and their
+ * quantities with minimal waste.
+ *
+ * @author Stewart Fritts <stewart.fritts@gmail.com>
+ */
+class OptimizeLinearCuts {
+    //put your code here
+}
